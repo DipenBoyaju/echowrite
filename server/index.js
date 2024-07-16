ddf239b2e27ac64544678e3457e95ae1f6c58525
Dipen <dipendraboyaju222@gmail.com>
2024-07-16 12:40:00 +0545

    b

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+const Mongo_db = process.env.DATABASE_URL;
+
+mongoose.connect(Mongo_db)
+  .then(() => {
+    console.log('Database is Connected');
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+
+app.listen(PORT, () => {
+  console.log(`server live at port ${PORT}`);
 })
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Server</h1>');
+});
+
+
+
+
